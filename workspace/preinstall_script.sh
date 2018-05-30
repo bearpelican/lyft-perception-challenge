748ebdff3c725c92947980c3bb6dc0bc2cdf734c
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-05-30 14:49:07 -0700

    Using cv2 encoder for faster image processing

--- workspace/preinstall_script.sh
+++ workspace/preinstall_script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # May need to uncomment and update to find current packages
-# apt-get update
+apt-get update
 
 # Required for demo script! #
+pip install --upgrade pip
 pip install scikit-video
+pip install opencv-python
 
 # Add your desired packages for each workspace initialization
 #          Add here!          #
+conda install pytorch torchvision cuda91 -c pytorch -y
+# wget https://s3-us-west-2.amazonaws.com/ashaw-fastai-imagenet/600urn-36-resnet-softmax-nocrop-tmp.h5
+wget https://s3-us-west-2.amazonaws.com/ashaw-fastai-imagenet/600urn-38-wide-crop-eval-nocrop-tmp-2.h5
