3b6166f7e9b1229a479e73a0e8398bcdffbb4629
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-06-05 17:15:32 -0400

    Training best model on test

---
+++ workspace/Example/demo-submission-12-last.py
@@ -0,0 +1,367 @@
+import sys, skvideo.io, json, base64
+import numpy as np
+from PIL import Image
+from io import BytesIO, StringIO
+from pathlib import Path
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import resnet18, resnet34, resnet50, vgg11_bn
+torch.backends.cudnn.benchmark=True
+
+# model_path = str(Path.cwd()/'600urn-19-weights.h5')
+model_path = str(Path.cwd()/'600urn-49_19-384-test-sync.h5')
+# model_path = str(Path.cwd()/'600urn-48_f1-384-test-sync-2.h5')
+
+'''
+class Unet50Mod(nn.Module):
+    def __init__(self, f=resnet50):
+        super().__init__()
+        m_base, lr_cut = get_base(f)
+        self.rn = m_base
+        self.lr_cut = lr_cut
+        self.sfs = [SaveFeatures(self.rn[i]) for i in [2,4,5,6]]
+        self.up1 = UnetBlock(2048,1024,1024)
+        self.up2 = UnetBlock(1024,512,512)
+        self.up3 = UnetBlock(512,256,256)
+        self.up4 = UnetBlock(256,64,256)
+        self.up5 = UnetBlock(256,32,32)
+        self.up6 = nn.ConvTranspose2d(32, 3, 1)
+        self.x_skip = nn.Sequential(
+            nn.Conv2d(3,32,1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        
+    def forward(self,x):
+        x_skip = self.x_skip(x)
+        x = F.relu(self.rn(x), inplace=True)
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x, x_skip)
+        x = self.up6(x)
+        return torch.squeeze(x)
+    
+    def close(self):
+        for sf in self.sfs: sf.remove()
+            
+class Unet34Mod(nn.Module):
+    def __init__(self, f=resnet34):
+        super().__init__()
+        m_base, lr_cut = get_base(f)
+        self.rn = m_base
+        self.lr_cut = lr_cut
+        self.sfs = [SaveFeatures(self.rn[i]) for i in [2,4,5,6]]
+        self.up1 = UnetBlock(512,256,256)
+        self.up2 = UnetBlock(256,128,256)
+        self.up3 = UnetBlock(256,64,256)
+        self.up4 = UnetBlock(256,64,256)
+        self.up5 = UnetBlock(256,16,16)
+        self.up6 = nn.ConvTranspose2d(16, 3, 1)
+        self.x_skip = nn.Sequential(
+            nn.Conv2d(3,16,1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True)
+        )
+        
+    def forward(self,x):
+        x_skip = self.x_skip(x)
+        x = F.relu(self.rn(x), inplace=True)
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x, x_skip)
+        x = self.up6(x)
+        return torch.squeeze(x)
+    
+    def close(self):
+        for sf in self.sfs: sf.remove()
+        
+'''
+class Unet34Mod(nn.Module):
+    def __init__(self, out=3, f=resnet34):
+        super().__init__()
+        m_base, lr_cut = get_base(f)
+        self.rn = m_base
+        self.lr_cut = lr_cut
+        self.sfs = [SaveFeatures(self.rn[i]) for i in [2,4,5,6]]
+        self.up1 = UnetBlock(512,256,256)
+        self.up2 = UnetBlock(256,128,256)
+        self.up3 = UnetBlock(256,64,128)
+        self.up4 = UnetBlock(128,64,64)
+        self.up5 = UnetBlock(64,32,32)
+        self.up6 = nn.ConvTranspose2d(32, out, 1)
+        self.x_skip = nn.Sequential(
+            nn.Conv2d(out,32,1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        
+    def forward(self,x):
+        x_skip = self.x_skip(x)
+        x = self.rn(x)
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x, x_skip)
+        x = self.up6(x)
+        return torch.squeeze(x)
+    
+    def close(self):
+        for sf in self.sfs: sf.remove()
+    
+from torchvision.models import vgg11_bn
+def children(m): return m if isinstance(m, (list, tuple)) else list(m.children())
+def vgg11(pre): return children(vgg11_bn(pre))[0]
+
+class Unet11Mini(nn.Module):
+    def __init__(self, f=vgg11):
+        super().__init__()
+        m_base, lr_cut = get_base(f)
+        self.rn = m_base
+        self.lr_cut = lr_cut
+        self.sfs = [SaveFeatures(self.rn[0][i]) for i in [2,6,13,20,27]]
+        self.up0 = UnetBlock(512,512,256)
+        self.up1 = UnetBlock(256,512,256)
+        self.up2 = UnetBlock(256,256,256)
+        self.up3 = UnetBlock(256,128,128)
+        self.up4 = UnetBlock(128,64,64)
+        self.up5  = nn.Conv2d(64,2,1)
+        
+    def forward(self,x):
+        x = F.relu(self.rn(x))
+        x = self.up0(x, self.sfs[4].features)
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x)
+        return x
+    
+class Unet34(nn.Module):
+    def __init__(self, f=resnet34):
+        super().__init__()
+        m_base, lr_cut = get_base(f)
+        self.rn = m_base
+        self.lr_cut = lr_cut
+        self.sfs = [SaveFeatures(self.rn[i]) for i in [2,4,5,6]]
+        self.up1 = UnetBlock(512,256,256)
+        self.up2 = UnetBlock(256,128,256)
+        self.up3 = UnetBlock(256,64,256)
+        self.up4 = UnetBlock(256,64,256)
+        self.up5 = UnetBlock(256,3,16)
+        self.up6 = nn.ConvTranspose2d(16, 3, 1)
+        
+    def forward(self,x):
+        inp = x
+        x = F.relu(self.rn(x), inplace=True)
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x, inp)
+        x = self.up6(x)
+        return torch.squeeze(x)
+    
+    def close(self):
+        for sf in self.sfs: sf.remove()
+            
+# ## Models
+class SaveFeatures():
+    features=None
+    def __init__(self, m): self.hook = m.register_forward_hook(self.hook_fn)
+    def hook_fn(self, module, input, output): self.features = output
+    def remove(self): self.hook.remove()
+
+class UnetBlock(nn.Module):
+    def __init__(self, up_in, x_in, n_out):
+        super().__init__()
+        up_out = x_out = n_out//2
+        self.x_conv  = nn.Conv2d(x_in,  x_out,  1)
+        self.tr_conv = nn.ConvTranspose2d(up_in, up_out, 2, stride=2)
+        self.bn = nn.BatchNorm2d(n_out)
+        
+    def forward(self, up_p, x_p):
+        up_p = self.tr_conv(up_p)
+        x_p = self.x_conv(x_p)
+        cat_p = torch.cat([up_p,x_p], dim=1)
+        return self.bn(F.relu(cat_p, inplace=True))
+    
+class UnetModel():
+    def __init__(self,model,name='unet'):
+        self.model,self.name = model,name
+
+    def get_layer_groups(self, precompute):
+        if isinstance(self.model, FP16):
+            model = self.model.module
+        else:
+            model = self.model
+        lgs = list(split_by_idxs(children(model.rn), [model.lr_cut]))
+        return lgs + [children(model)[1:]]
+
+def cut_model(m, cut):
+    return list(m.children())[:cut] if cut else [m]
+
+def get_base(f):
+    cut,lr_cut = model_meta[f]
+    layers = cut_model(f(False), cut)
+    return nn.Sequential(*layers), lr_cut
+
+model_meta = {
+    resnet34:[8,6],
+    resnet50:[8,6],
+    vgg11:[0,13]
+}
+
+
+class FP16(nn.Module):
+    def __init__(self, module): 
+        super(FP16, self).__init__()
+        self.module = batchnorm_to_fp32(module.half())
+        
+    def forward(self, input): 
+        return self.module(input.half())
+    
+    def load_state_dict(self, *inputs, **kwargs):
+        self.module.load_state_dict(*inputs, **kwargs)
+
+    def state_dict(self, *inputs, **kwargs):
+        return self.module.state_dict(*inputs, **kwargs)
+    
+def batchnorm_to_fp32(module):
+    '''
+    BatchNorm layers to have parameters in single precision.
+    Find all layers and convert them back to float. This can't
+    be done with built in .apply as that function will apply
+    fn to all modules, parameters, and buffers. Thus we wouldn't
+    be able to guard the float conversion based on the module type.
+    '''
+    if isinstance(module, nn.modules.batchnorm._BatchNorm):
+        module.float()
+    for child in module.children():
+        batchnorm_to_fp32(child)
+    return module
+
+# Load model
+m = Unet34()
+
+cuda_enabled = torch.cuda.is_available()
+if cuda_enabled:
+    m = FP16(m).cuda().half()
+    m = m.cuda()
+    m.load_state_dict(torch.load(model_path))
+else:
+    m.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
+
+
+# Process video
+file = sys.argv[-1]
+# file = 'test_video.mp4'
+
+if file == 'demo.py':
+  print ("Error loading video")
+  quit
+
+# Define encoder function
+# def encode(array):
+# 	pil_img = Image.fromarray(array)
+# 	buff = BytesIO()
+# 	pil_img.save(buff, format="PNG")
+# 	return base64.b64encode(buff.getvalue()).decode("utf-8")
+import cv2
+def encode(array):
+    retval, buffer = cv2.imencode('.png', array)
+    return base64.b64encode(buffer).decode("utf-8")
+
+video = skvideo.io.vread(file)
+
+imagenet_stats = torch.cuda.FloatTensor([[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]])
+def normalize(x):
+    if x.mean() > 1:
+        x.div_(255.0)
+    m,s = imagenet_stats
+    x.sub_(m.view(-1, 1, 1))
+    x.div_(s.view(-1, 1, 1))
+    return x
+
+def crop_bg(x):
+    # Original
+    h = x.shape[2]
+    top = int(h/3.75)
+    bot = int(h*.9 + h/150)
+    return x[:,:,top:bot,:]
+
+def pad(x):
+    # Original
+    b,c,w,h = x.shape
+    if h%32 == 0:
+        return x, 0
+    pad_right=32-h%32
+    if pad_right:
+        x = F.pad(x, (0,pad_right,0,0), 'constant', 0)
+    return x, pad_right
+    
+def undo(idx):
+    idx
+    idx = F.pad(idx, (0,0,226,54), "constant", 0)
+
+def preprocess(video):
+#     f1 = video[:,200:520,:,:]
+    f1 = np.rollaxis(video, 3, 1)
+    f1 = torch.from_numpy(f1).float().cuda()
+    f1 = crop_bg(f1)
+    f1 = normalize(f1)
+    return f1.contiguous()
+
+video = preprocess(video)
+# n = video.shape[0]
+# video = torch.cat((video[:int(n/2)], video[int(n/2)+1:]))
+answer_key = {}
+bs = 8
+
+import gc
+gc.collect()
+res = []
+# mismatched_idxs = list(range(15,44)) + list(range(200,750))
+mismatched_idxs = []
+m.eval()
+for i in range(0,video.shape[0],bs):
+    x = video[i:i+bs]
+    
+    x,p = pad(x)
+    preds = m(torch.autograd.Variable(x))
+    
+    # Indexes
+    mx,idx = torch.max(preds, 1)
+    if p > 0:
+        idx = idx[:,:,:-p]
+    idx = F.pad(idx, (0,0,160,56), "constant", 0)
+    for frame in idx:
+        frame = frame.data.cpu().numpy()
+        binary_car_result = (frame==1).astype('uint8')
+        binary_road_result = (frame==2).astype('uint8')
+        res.append([encode(binary_car_result), encode(binary_road_result)])
+    
+    # Preds
+#     preds = F.sigmoid(preds)
+#     if p > 0:
+#         preds = preds[:,:,:,:-p]
+#     preds = F.pad(preds, (0,0,160,56,0,0), "constant", 0)
+#     for frame in preds:
+#         frame = frame.data.cpu().float().numpy()
+#         binary_car_result = (frame[0]>0.5).astype('uint8')
+#         binary_road_result = (frame[1]>0.5).astype('uint8')
+#         res.append([encode(binary_car_result), encode(binary_road_result)])
+
+for i in range(1, len(res)+1):
+    s_idx = i if i in mismatched_idxs else i-1
+#     s_idx = i-1
+    answer_key[i] = res[s_idx]
+
+# Print output in proper json format
+print (json.dumps(answer_key))
